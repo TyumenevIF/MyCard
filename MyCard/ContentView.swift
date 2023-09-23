54069c90cc4c828aefda9c0c4ffa1ff12296626a
Ilyas Tyumenev <tumenoff@mail.ru>
2023-09-23 19:23:31 +0300

    Add font

--- MyCard/ContentView.swift
+++ MyCard/ContentView.swift
@@ -11,16 +11,10 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             Color(red: 0.09, green: 0.63, blue: 0.52).edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-            VStack {
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundStyle(.tint)
-                Text("Ilyas")
-                    .font(Font.custom("Pacifico-Regular", size: 40))
-                    .bold()
-                    .foregroundColor(.white)
-            }
-            .padding()
+            Text("Ilyas")
+                .font(Font.custom("Pacifico-Regular", size: 40))
+                .bold()
+                .foregroundColor(.white)
         }
     }
 }
