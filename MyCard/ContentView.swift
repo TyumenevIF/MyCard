70bd614ede32d50fa8b43a4c1eaa563aada2554e
Ilyas Tyumenev <tumenoff@mail.ru>
2023-09-23 19:45:46 +0300

    Add avatar, text

--- MyCard/ContentView.swift
+++ MyCard/ContentView.swift
@@ -11,10 +11,28 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             Color(red: 0.09, green: 0.63, blue: 0.52).edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-            Text("Ilyas")
-                .font(Font.custom("Pacifico-Regular", size: 40))
-                .bold()
-                .foregroundColor(.white)
+            VStack {
+                Image("Ilyas")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 150, height: 150)
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    .overlay(
+                        Circle().stroke(Color.white, lineWidth: 5)
+                    )
+                Text("Tyumenev Ilyas")
+                    .font(Font.custom("Pacifico-Regular", size: 40))
+                    .bold()
+                    .foregroundColor(.white)
+                Text("iOS Developer")
+                    .font(.system(size: 25))
+                    .foregroundColor(.white)
+                Divider()
+                RoundedRectangle(cornerRadius: 25)
+                    .fill(Color.white)
+                    .frame(height: 50)
+                    .overlay(Text("+7 903 370 4556"))                
+            }
         }
     }
 }
